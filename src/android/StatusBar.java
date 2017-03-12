78ffc4e7340fa265384306ffe4df4635ee83c615
Leslie Bruder <lescold@gmail.com>
2017-03-12 15:16:35 -0400

    pub
    fixing bug that causes nav to stay showing

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -114,8 +114,12 @@ public class StatusBar extends CordovaPlugin {
                     // use KitKat here to be aligned with "Fullscreen"  preference
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                         int uiOptions = window.getDecorView().getSystemUiVisibility()
+                                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE
 
                         window.getDecorView().setSystemUiVisibility(uiOptions);
                     }
